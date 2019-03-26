ea019b43070e8b332f6758d4c5431ed5067378ba
Ben Y <GitYBenY-a4h1@mailnicks.org>
2019-03-26 10:47:48 -0700

    remove selection while loop

--- scripts/rofiA.sh
+++ scripts/rofiA.sh
@@ -9,31 +9,30 @@ Listen from Microphone Port
 Kill Wineserver
 Suspend Computer" | rofi -dmenu -mesg "What would you like to do?" -i -p "")
 
-while [ "$choice" != "Cancel" ]; do
-    case $choice in
-        Launch\ Steam\ \(Wine\) )
-            wine64 ~/.wine/drive_c/Program\ Files\ \(x86\)/Steam/Steam.exe -no-cef-sandbox
-        exit;;
+case $choice in
+    Launch\ Steam\ \(Wine\) )
+        wine64 ~/.wine/drive_c/Program\ Files\ \(x86\)/Steam/Steam.exe -no-cef-sandbox
+    exit;;
 
-        Launch\ Dolphin\ Emulator\ \(Wine\) )
-            wine64 /media/ben/ExtraDrive1/Programs/Dolphin/Dolphin.exe
-         exit;;
+    Launch\ Dolphin\ Emulator\ \(Wine\) )
+        wine64 /media/ben/ExtraDrive1/Programs/Dolphin/Dolphin.exe
+     exit;;
 
-        Kill\ Wineserver )
-            wineserver -k
-        exit;;
+    Kill\ Wineserver )
+        wineserv=$(echo -e "No\nYes" | rofi -dmenu -mesg "Are you sure you want to kill the wineserver?" -i -p "")
+            [ "$wineserv" == "Yes" ] && wineserver -k
+    exit;;
 
-        Listen\ from\ Microphone\ Port )
-            listen=$(echo -e "Turn on Listening\nTurn off Listening" | rofi -dmenu -msg "MAKE SURE THE INPUT DEVICE'S VOLUME IS VERY LOW\nOTHERWISE YOU RISK RUINING THE MICROPHONE" -i -p "")
-
+    Listen\ from\ Microphone\ Port )
+        listen=$(echo -e "Turn on Listening\nTurn off Listening" | rofi -dmenu -mesg "MAKE SURE THE INPUT DEVICE'S VOLUME IS VERY LOW" -i -p "")
             [ "$listen" == "Turn on Listening" ] && pactl load-module module-loopback
             [ "$listen" == "Turn off Listening" ] && pactl unload-module module-loopback
-        exit;;
+    exit;;
 
-        Suspend\ Computer )
-            systemctl suspend
-        exit;;
+    Suspend\ Computer )
+        sus=$(echo -e "No\nYes" | rofi -dmenu -mesg "Suspend Computer?" -i -p "")
+            [ "$sus" == "Yes" ] && systemctl suspend
+    exit;;
 
-    esac
-done
+esac
 
