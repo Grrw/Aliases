d86630e0743dee212d2b88d5cbd1f25b2333f39d
Ben Y <GitYBenY-a4h1@mailnicks.org>
2019-04-29 18:57:19 -0700

    use sed to modify filenames

--- moc/moc-display.sh
+++ moc/moc-display.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-# short script to list mocp song currently playing
+# sed commands:
+# remove "Music/" and anything before it
+# replace any numbers followed by a whitespace and pipe (e.g. "03 |") with " -"
+    # There is a whitespace after |, so this makes " - "
+# replace any slashes with ": "
+# remove all characters following a period
 
-name=$(mocp -Q %file)
+# This parses filenames from /home/ into something nice to read
+# ...but they must follow this syntax:
+#  /Music/Artist/Album/number | Songname.format
 
-IFS='|' read -a ARR <<< "$name"
+name=$(mocp -Q %file | sed "s/.*Music\///g;s/\/[0-9]*\s|/ -/g;s/\//: /g;s/\..*//g")
 
-notify-send "Now Playing" "$(printf "${ARR[1]}\n" && printf "Time Left: " && mocp -Q %tl)"
-
-
-unset IFS
-
+notify-send "Time Elapsed: `mocp -Q %ct`/`mocp -Q %tt`" "$name"
 
