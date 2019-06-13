dbffe2211990b5d0190446063626b70842c3e2bf
Ben Y <GitYBenY-a4h1@mailnicks.org>
2019-06-13 15:04:57 +0300

    scripts/sysBrightnessDN.sh
    compatibiltiy script for KDE hotkeys
    You cannot pass arguments to scripts

---
+++ scripts/sysBrightnessDN.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# this only exists as compatibility with KDE custom hotkeys
+# it is impossible to have arguments for shell scripts
+/home/ben/Documents/alias/scripts/./sysBrightness.sh dn
